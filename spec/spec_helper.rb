3ea6a477b6d571267142f9a78adfe02a3d4199e2
Stefano Savanelli <stefsava@users.noreply.github.com>
2015-04-04 23:25:23 +0200

    Update spec_helper.rb
    for circleci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
-require 'simplecov'
-SimpleCov.start 'rails'
+if ENV['CIRCLE_ARTIFACTS']
+  require 'simplecov'
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+else
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
 
 ENV['RAILS_ENV'] = 'test'
 
